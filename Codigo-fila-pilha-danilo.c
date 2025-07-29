6d4915f06597557583e6cb1db5285ef195bcedf2
JulioBrasil1 <166073238+JulioBrasil1@users.noreply.github.com>
2025-07-29 08:37:03 -0300

    Fila_laboratorio.c
    Código modificado do professor Danilo para ser agora uma fila de laboratório.

--- Codigo-fila-pilha-danilo.c
+++ Codigo-fila-pilha-danilo.c
@@ -1,242 +1,312 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
 
-#define TYPE_PILHA 1 
-#define TYPE_FILA 2
+#define TAM 15
+
 
 typedef struct tagFila {
-    int        type;// tipo da estrutura Pilha ou Fila
-    char    *buffer;// Buffer para guardar os elementos
-    char     *first;// ponteiro para primeiro elemento
-    char      *last;// ponteiro para ultimo
-    int        size;// tamanho da fila
-    int sizeElement;// tamanho do elemento da fila
-    int  maxElement;// numero maximo de elementos
+    char    *buffer;// Buffer para guardar os elementos
+    char     *first;// ponteiro para primeiro elemento
+    char      *last;// ponteiro para ultimo
+    int        size;// tamanho da fila
+    int sizeElement;// tamanho do elemento da fila
+    int  maxElement;// numero maximo de elementos
+    bool preferencial;// determina se a fila e de atendimento preferencial ou não 
 } TFila;
 
-bool Fila_create(TFila *fila, int sizeElement, int max, int type);
+TFila fila_normal;
+TFila fila_preferencial;
+char fila_next; //Determina o tipo de paciente a ser chamado, 1 para preferencial, 2 para normal
+char n_filas_normais = 0;//Quantidade de filas normais criadas
+char n_filas_preferenciais = 0;//Quantidade de filas preferenciais criadas
+
+bool Fila_create(TFila *fila, int sizeElement, int max, bool preferencial);
 void Fila_destroy(TFila *fila);
 bool Fila_put(TFila *fila,char *data);
 bool Fila_get(TFila *fila,char *data);
 bool Fila_isEmpty(TFila *fila);
 bool Fila_isFull(TFila *fila);
 int Fila_size(TFila *fila);
+bool Menu();
+bool EscolherNomeDessaFuncaoDepois(char escolha);
 
-bool Fila_create(TFila *fila, int sizeElement, int max, int type)
-{
-    // verifica se paramentros da funçao sao validos
-    if(fila == NULL){
-        return false;
-    }
-    if((sizeElement==0)||(max==0)){
-        return false;
-    }
-    // aloca area para a fila
-    if( (fila->buffer = malloc(sizeElement *max))==NULL){
-        return false;
-    }
-    // inicializa propriedades da fila
-    fila->size = 0;
-    fila->sizeElement = sizeElement;
-    fila->maxElement = max;
-    fila->first = fila->buffer;
-    fila->last  = fila->buffer;
-    fila->size = 0;
-    fila->type = type;
-    
-    return true;
+int main(){
+    bool continuar = true;
+    
+    while(continuar == true){
+        continuar = Menu();
+    }
+    
+    printf("\nSessão encerrada, adeus.\n");
+    
+    return 0;
+}
+
+bool Fila_create(TFila *fila, int sizeElement, int max, bool preferencial){
+    // verifica se paramentros da funçao sao validos
+    if(fila == NULL){
+        return false;
+    }
+    if((sizeElement==0)||(max==0)){
+        return false;
+    }
+    // aloca area para a fila
+    if( (fila->buffer = malloc(sizeElement *max))==NULL){
+        return false;
+    }
+    // inicializa propriedades da fila
+    fila->size = 0;
+    fila->sizeElement = sizeElement;
+    fila->maxElement = max;
+    fila->first = fila->buffer;
+    fila->last  = fila->buffer;
+    fila->size = 0;
+    fila->preferencial = preferencial;
+   
+    return true;
 }
 
 void Fila_destroy(TFila *fila){
-    // libera a area da fila
-    free(fila->buffer);
-    // zera propriedades
-    fila->size = 0;
-    fila->sizeElement = 0;
-    fila->maxElement = 0;
-    fila->first = NULL;
-    fila->last  = NULL;
-    fila->size = 0;
+    // libera a area da fila
+    free(fila->buffer);
+    // zera propriedades
+    fila->size = 0;
+    fila->sizeElement = 0;
+    fila->maxElement = 0;
+    fila->first = NULL;
+    fila->last  = NULL;
+    fila->size = 0;
 }
-    
 
 bool Fila_put(TFila *fila,char *data){
-    if(data == NULL){
-        return false;
-    }
-    if(Fila_isFull(fila)==false){
-        // copia o dado para a Fila
-        memcpy(fila->last,data,fila->sizeElement);
-        // incremento o ponteiro do ultimo
-        if (fila->size < fila->maxElement){
-            // Incrementa ponteiro
-            fila->last += fila->sizeElement;
-            // checa se nao deve circular o ponteiro
-            if(fila->type == TYPE_FILA){
-                if(fila->last >= fila->buffer + fila->maxElement*fila->sizeElement)
-                   fila->last = fila->buffer;
-            }
-            
-            // increnta o numero de elementos na fila
-            fila->size++;
-            return true;
-        }
-    }
-    return false;
-    
+    if(data == NULL){
+        return false;
+    }
+    if(Fila_isFull(fila)==false){
+        // copia o dado para a Fila
+        memcpy(fila->last,data,fila->sizeElement);
+        // incremento o ponteiro do ultimo
+        if (fila->size < fila->maxElement){
+            // Incrementa ponteiro
+            fila->last += fila->sizeElement;
+            if(fila->last >= fila->buffer + fila->maxElement*fila->sizeElement){
+                fila->last = fila->buffer;
+            }
+            // increnta o numero de elementos na fila
+            fila->size++;
+            return true;
+        }
+    }
+    return false;
+   
 }
 bool Fila_get(TFila *fila,char *data){
 
-    if(data == NULL){
-        return false;
-    }
-    if (fila->type == TYPE_PILHA){  
-        if(Fila_isEmpty(fila)==false){
-            // Decrementa o ponteiro do ultimo
-            if (fila->size > 0){
-                // decrementa o last
-                fila->last -= fila->sizeElement;
-                memcpy(data,fila->last, fila->sizeElement);
-                // decrementa o numero de elementos na fila
-                fila->size--;
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
-        return false;
-    }
-    else{
-        // Caso Fila
-        if(Fila_isEmpty(fila)==false){
-            // copia o dado para a Fila
-            memcpy(data,fila->first, fila->sizeElement);
-            // incremento o ponteiro do ultimo
-            if (fila->size > 0){
-                // Incrementa ponteiro
-                fila->first += fila->sizeElement;
-                // checa se nao deve circular o ponteiro
-                if(fila->first>= fila->buffer + fila->maxElement*fila->sizeElement)
-                   fila->first = fila->buffer;
-                
-                // increnta o numero de elementos na fila
-                fila->size--;
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
-        return false;
-    }
+    if(data == NULL){
+        return false;
+    }
+    
+    if(Fila_isEmpty(fila)==false){
+        // copia o dado para a Fila
+        memcpy(data,fila->first, fila->sizeElement);
+        // incremento o ponteiro do ultimo
+        if (fila->size > 0){
+            // Incrementa ponteiro
+            fila->first += fila->sizeElement;
+            // checa se nao deve circular o ponteiro
+            if(fila->first>= fila->buffer + fila->maxElement*fila->sizeElement)
+            fila->first = fila->buffer;
+               
+            // increnta o numero de elementos na fila
+            fila->size--;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+        return false;
 }
+
 bool Fila_isEmpty(TFila *fila){
-    if(fila->size == 0)
-        return true;
-    return false;
+    if(fila->size == 0)
+        return true;
+    return false;
 }
 bool Fila_isFull(TFila *fila){
-    if(fila->size == fila->maxElement){
-        return true;
-    }
-    return false;
+    if(fila->size == fila->maxElement){
+        return true;
+    }
+    return false;
 }
 
 int Fila_size(TFila *fila){
-    return fila->size;
+    return fila->size;
 }
 
 void Fila_dump(TFila *fila){
-    char *current,*j;
-    int i;
-    printf("*************************\n");
-    printf("buffer = %04x\n", fila->buffer);
-    printf("SizeElem = %d\n",fila->sizeElement);
-    printf("Size     = %d\n",fila->size);
-    printf("Max      = %d\n",fila->maxElement);
-    printf("first  = %04x\n",fila->first);
-    printf("Last   = %04x\n",fila->last);
-    printf("-----------------------\n");
-    current = fila->first;
-    for( i =0; i< fila->size;i++){
-        printf("Elemento [%d]\n",i);
-        for(j=current; j < current + fila->sizeElement;j++){
-            printf("%04x : %02x\n",j,(*j));
-        }
-        current += fila->sizeElement;
-        if(current>= fila->buffer + fila->maxElement*fila->sizeElement)
-           current = fila->buffer; 
-        printf("-----------------------\n");
-    }
+    char *current,*j;
+    int i;
+    printf("*************************\n");
+    printf("buffer = %04x\n", fila->buffer);
+    printf("SizeElem = %d\n",fila->sizeElement);
+    printf("Size     = %d\n",fila->size);
+    printf("Max      = %d\n",fila->maxElement);
+    printf("first  = %04x\n",fila->first);
+    printf("Last   = %04x\n",fila->last);
+    printf("-----------------------\n");
+    current = fila->first;
+    for( i =0; i< fila->size;i++){
+        printf("Elemento [%d]\n",i);
+        for(j=current; j < current + fila->sizeElement;j++){
+            printf("%04x : %02x\n",j,(*j));
+        }
+        current += fila->sizeElement;
+        if(current>= fila->buffer + fila->maxElement*fila->sizeElement)
+           current = fila->buffer;
+        printf("-----------------------\n");
+    }
 }
 
-int main()
-{
-    TFila  fila;
-    int a,i;
-    bool result;
-    
-    Fila_create(&fila, sizeof(int), 5, TYPE_PILHA);
+bool Menu(){
+    char escolha;
+    bool continuar;
+    
+    printf("Escolha uma opção:\n");
+    printf("\n1 - Criar fila");
+    printf("\n2 - Adicionar paciente");
+    printf("\n3 - Chamar paciente");
+    printf("\n4 - liberar paciente");
+    printf("\n5 - encerrar fila");
+    printf("\n6 - encerrar sessão\n");
+    printf("\nEscolha: ");
+    scanf(" %c", &escolha);
+    
+    continuar = EscolherNomeDessaFuncaoDepois(escolha);
+    
+    return continuar;
+}
 
-    printf("Teste Basico\n");
-    
-    a=1;
-    result = Fila_put(&fila,&a);
-    printf("Resultado = %s\n",result ? "true" : "false");
-    Fila_dump(&fila);
-    
-    a=2;
-    result = Fila_put(&fila,&a);
-    printf("Resultado = %s\n",result ? "true" : "false");
-    Fila_dump(&fila);
-    
-    a=3;
-    result = Fila_put(&fila,&a);
-    printf("Resultado = %s\n",result ? "true" : "false");
-    Fila_dump(&fila);
-
-    a=4;
-    result = Fila_put(&fila,&a);
-    printf("Resultado = %s\n",result ? "true" : "false");
-    Fila_dump(&fila);
-
-    a=5;
-    result = Fila_put(&fila,&a);
-    printf("Resultado = %s\n",result ? "true" : "false");
-    Fila_dump(&fila);
-
-    result = Fila_get(&fila,&a);
-    printf("data =%d\n",a);
-    printf("Resultado = %s\n",result ? "true" : "false");
-    Fila_dump(&fila);
-
-
-    a=6;
-    result = Fila_put(&fila,&a);
-    printf("Resultado = %s\n",result ? "true" : "false");
-    Fila_dump(&fila);
-    
-    for(i=0;i<fila.maxElement;i++){
-        result = Fila_get(&fila,&a);
-        printf("data =%d\n",a);
-        printf("Resultado = %s\n",result ? "true" : "false");
-        Fila_dump(&fila);
-    }
-
-    
-    
-    Fila_destroy(&fila);
-
-    return 0;
+bool EscolherNomeDessaFuncaoDepois(char escolha){
+    bool result;
+    char escolha_fila;
+    char data = 1; //variável temporária apenas para testar o put e o get das filas
+    
+    switch(escolha){
+        case '1':
+            if(n_filas_normais == 1 && n_filas_preferenciais == 1){
+                printf("\nNúmero máximo de filas já foi alcançado, não é possível criar mais filas\n");
+                break;
+            }
+            printf("\nEscolha o tipo de fila");
+            printf("\n1 - normal\n2 - preferencial\n");
+            printf("\nEscolha: ");
+            scanf(" %d", &escolha_fila);
+            if(escolha_fila == 1){
+                if(n_filas_normais == 1){
+                    printf("\nNúmero máximo de filas normais já foi alcançada, não é possível criar mais\n");
+                    break;
+                }
+                result = Fila_create(&fila_normal, sizeof(int), TAM, false);
+                if(result == true){
+                    printf("\nFila criada com sucesso!\n");
+                    n_filas_normais++;
+                }
+                else{
+                    printf("\nErro ao criar fila, tente novamente\n");  
+                }  
+            }
+            else if(escolha_fila == 2){
+                if(n_filas_preferenciais == 1){
+                    printf("\nNúmero máximo de filas preferenciais já foi alcançado, não é possível criar mais\n");
+                }
+                result = Fila_create(&fila_preferencial, sizeof(int), TAM, true);
+                if(result == true){
+                    printf("\nFila criada com sucesso!\n");
+                    n_filas_preferenciais++;
+                }
+                else{
+                    printf("\nErro ao criar fila, tente novamente\n");  
+                }
+            }
+            else{
+                printf("\nOpção inválida, tente novamente\n");
+            }
+            break;
+        case '2':
+            printf("\nEscolha o tipo de fila para adicionar o paciente");
+            printf("\n1 - normal\n2 - preferencial\n");
+            printf("\nEscolha: ");
+            scanf(" %d", &escolha_fila);
+            if(escolha_fila == 1){
+                if(fila_normal.maxElement == 15){
+                    printf("\nA fila está cheia, não é possível adicionar pacientes.\n");
+                    break;
+                }
+                result = Fila_put(&fila_normal, &data);
+                if(result == true){
+                    printf("\nPaciente adicionado com sucesso.\n");
+                }
+                else{
+                    printf("\nErro ao adicionar paciente, tente novamente\n");
+                }
+            }
+            else if(escolha_fila == 2){
+                if(fila_preferencial.maxElement == 15){
+                    printf("\nA fila está cheia, não é possível adicionar pacientes.\n");
+                    break;
+                }
+                result = Fila_put(&fila_preferencial, &data);
+                if(result == true){
+                    printf("\nPaciente adicionado com sucesso.\n");
+                }
+                else{
+                    printf("\nErro ao adicionar paciente, tente novamente\n");
+                }
+            }
+            break;
+        case '3':
+            break;
+        case '4':
+            break;
+        case '5':
+            if(n_filas_normais == 0 && n_filas_preferenciais == 0){
+                printf("\nNão há filas para serem encerradas\n");
+                break;
+            }
+            printf("\nEscolha a fila a ser encerrada");
+            printf("\n1 - fila normal\n2 - fila preferencial");
+            printf("\nEscolha: ");
+            scanf(" %d", &escolha_fila);
+            if(escolha_fila == 1){
+                if(n_filas_normais == 0){
+                    printf("\nNão há filas normais para serem encerradas\n");
+                    break;
+                }
+                Fila_destroy(&fila_normal);
+                printf("\nFila encerrada com sucesso.\n");
+                n_filas_normais--;
+            }
+            else if(escolha_fila == 2){
+                if(n_filas_preferenciais == 0){
+                    printf("\nNão há filas preferenciais para serem encerradas\n");
+                    break;
+                }
+                Fila_destroy(&fila_preferencial);
+                printf("\nFila encerrada com sucesso.\n");
+                n_filas_preferenciais--;
+            }
+            else{
+                printf("\nOpção inválida, tente novamemte\n");
+            }
+            break;
+        case '6':
+            return false;
+        default:
+            printf("\nOpção inválida, tente novamente\n");
+    }
+    
+    return true;
 }
